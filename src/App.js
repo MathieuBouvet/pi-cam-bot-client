252556d0b77b0180717d5fb4de79dc607200284c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-07 21:47:07 +0200

    Add a switch
    It will be used to start the camera

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import "material-icons/iconfont/material-icons.css";
-import { Navbar, Icon } from "react-materialize";
+import { Navbar, Icon, Switch } from "react-materialize";
 
 import "./App.css";
 
@@ -14,8 +14,10 @@ function App() {
           alignLinks="right"
           brand={<span className="brand-label">Py Cam Bot</span>}
           menuIcon={<Icon>menu</Icon>}
-          className="purple"
-        />
+          className="purple darken-4"
+        >
+          <Switch className="on-off-button" onLabel="on" offLabel="off" />
+        </Navbar>
       </header>
     </div>
   );
