caa32dab7666e9d9673042e477d954a4f982a3d2
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-08 00:32:52 +0200

    Create StartCamButton component

---
+++ src/StartCamButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./StartCamButton.css";
+
+const StartCamButton = () => <div>Start Cam Button</div>;
+
+export default StartCamButton;
