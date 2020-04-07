7f1a10acf3151ab9ce765e30af716259b870755a
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-08 00:02:29 +0200

    Implement starting and stopping the camera

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 import AppDisplay from "./AppDisplay";
+import useSwitch from "./switchHook";
 
 function App() {
-  return <AppDisplay />;
+  const [started, toggle, start] = useSwitch(false);
+  const handleOnOffClick = () => toggle();
+
+  const handleStartClick = () => start();
+  return (
+    <AppDisplay
+      started={started}
+      onOffClickHandler={handleOnOffClick}
+      startClickHandler={handleStartClick}
+    />
+  );
 }
 
 export default App;
--- src/AppDisplay.js
+++ src/AppDisplay.js
@@ -1,13 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import "material-icons/iconfont/material-icons.css";
-import { Navbar, Icon, Switch } from "react-materialize";
+import { Navbar, Icon, Switch, Button } from "react-materialize";
 
 import "./AppDisplay.css";
 
-const AppDisplay = (props) => (
+const AppDisplay = ({ started, onOffClickHandler, startClickHandler }) => (
   <div className="app teal lighten-5">
     <header>
       <Navbar
@@ -16,24 +17,55 @@ const AppDisplay = (props) => (
         menuIcon={<Icon>menu</Icon>}
         className="purple darken-4"
       >
-        <Switch className="on-off-button" onLabel="on" offLabel="off" />
+        <Switch
+          className="on-off-button"
+          onLabel="on"
+          offLabel="off"
+          onChange={onOffClickHandler}
+        />
       </Navbar>
     </header>
     <div className="content">
       <div className="video-wrapper">
-        <div className="video-container">
-          <iframe
-            title="cam-bot"
-            width="853"
-            height="480"
-            src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
+        <div className={"video-container"}>
+          {started && (
+            <iframe
+              title="cam-bot"
+              width="853"
+              height="480"
+              src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          )}
         </div>
+        {!started && (
+          <div className="cam-stopped grey">
+            <div className="start-cam">
+              <Button
+                className="purple darken-4"
+                floating
+                large
+                waves="light"
+                onClick={startClickHandler}
+              >
+                <Icon>power_settings_new</Icon>
+              </Button>
+              <div className="start-cam-text" onClick={startClickHandler}>
+                Demarrer le Robot
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   </div>
 );
+
+AppDisplay.propTypes = {
+  started: PropTypes.bool.isRequired,
+  onOffClickHandler: PropTypes.func.isRequired,
+  startClickHandler: PropTypes.func.isRequired,
+};
 
 export default AppDisplay;
