a0e4da862cdcbb5b309cc92d4b4aedc002d6b34a
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-07 21:13:43 +0200

    Create the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+import "material-icons/iconfont/material-icons.css";
+import { Navbar, Icon } from "react-materialize";
+
+import "./App.css";
 
 function App() {
-  return <div className="app">hello world ;)</div>;
+  return (
+    <div className="app">
+      <header>
+        <Navbar
+          alignLinks="right"
+          brand={<span className="brand-label">Py Cam Bot</span>}
+          menuIcon={<Icon>menu</Icon>}
+          className="purple"
+        />
+      </header>
+    </div>
+  );
 }
 
 export default App;
