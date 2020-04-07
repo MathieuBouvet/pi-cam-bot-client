06310a7e1c4d4e613999b1628628edc60a8598a6
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-07 22:19:50 +0200

    Add a background color

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app teal lighten-5">
       <header>
         <Navbar
           alignLinks="right"
