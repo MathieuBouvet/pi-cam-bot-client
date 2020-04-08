bd23e7b9122aca2bd560e88c932d483bf886346d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-08 21:59:17 +0200

    Implement useReducer for arrowPressed state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 import AppDisplay from "./components/AppDisplay";
 import useSwitch from "./hooks/switchHook";
 
+const initArrowPressed = () => {
+  return {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false,
+  };
+};
+
+const reducer = (state, action) => {
+  if (!state[action.key]) {
+    return { ...state };
+  }
+  switch (action.type) {
+    case "keyDown":
+      return { ...state, [action.key]: true };
+    case "keyUp":
+      return { ...state, [action.key]: false };
+    default:
+      throw new Error("Invalid action type for the reducer");
+  }
+};
+
 function App() {
   const [started, toggle, start] = useSwitch(false);
   const [iframeLoading, setIframeLoading] = useState(false);
+
+  const [arrowPressed, dispatch] = useReducer(reducer, null, initArrowPressed);
+
   const handleOnOffClick = () => {
     toggle();
     setIframeLoading(!started);
