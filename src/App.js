839295a8f57a673c6a198b5544ec6b0872972498
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-08 22:39:09 +0200

    Add the keyboard event listeners

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 
 import AppDisplay from "./components/AppDisplay";
 import useSwitch from "./hooks/switchHook";
@@ -27,10 +27,27 @@ const reducer = (state, action) => {
 };
 
 function App() {
+  const handleKeyDown = (event) => {
+    if (!event.repeat) {
+      dispatch({ type: "keyDown", key: event.key });
+    }
+  };
+  const handleKeyUp = (event) => {
+    dispatch({ type: "keyUp", key: event.key });
+  };
   const [started, toggle, start] = useSwitch(false);
   const [iframeLoading, setIframeLoading] = useState(false);
 
   const [arrowPressed, dispatch] = useReducer(reducer, null, initArrowPressed);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
   const handleOnOffClick = () => {
     toggle();
