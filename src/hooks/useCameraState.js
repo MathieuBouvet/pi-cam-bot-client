d5b38036cab0b30a87f2185eba7129eb3035053c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-10 21:03:02 +0200

    Add a streamReady state to the camera
    For taking into account the setup time of mjpg streamer

--- src/hooks/useCameraState.js
+++ src/hooks/useCameraState.js
@@ -3,14 +3,20 @@ import { useReducer } from "react";
 const cameraStateReducer = (state, action) => {
   switch (action.type) {
     case "start-cam":
-      return { started: true, streamLoading: true };
+      return { started: true, streamReady: false, streamLoading: true };
     case "toggle-cam":
       return {
         started: !state.started,
+        streamReady: false,
         streamLoading: !state.started,
       };
     case "stream-loaded":
       return { ...state, streamLoading: false };
+    case "stream-ready":
+      return {
+        ...state,
+        streamReady: true,
+      };
     default:
       throw new Error("Invalid action type for cameraStateReducer");
   }
@@ -19,6 +25,7 @@ const cameraStateReducer = (state, action) => {
 const useCameraState = (initArg, initFunc) => {
   return useReducer(cameraStateReducer, {
     started: false,
+    streamReady: false,
     streamLoading: false,
   });
 };
