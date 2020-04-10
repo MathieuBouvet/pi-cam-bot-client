d1c1cf4e552002713d955cb22e64b96e3cdb8a44
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-10 21:03:34 +0200

    Send json data

--- src/helpers/backendRequests.js
+++ src/helpers/backendRequests.js
@@ -3,6 +3,9 @@ export const cameraStreamUrl = "http://localhost:8080/?action=stream";
 export const updateCamera = async (data) => {
   const init = {
     method: "PUT",
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
     body: JSON.stringify(data),
   };
   return fetch(backendUrl + "/camera", init);
