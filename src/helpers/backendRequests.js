04e934d1c75cdf0fc483f0951803b715d054ed61
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-23 17:07:12 +0200

    Extract init object logic in a function

--- src/helpers/backendRequests.js
+++ src/helpers/backendRequests.js
@@ -1,13 +1,15 @@
 export const backendUrl = "http://localhost:3001";
 export const cameraStreamUrl = "http://localhost:8080/?action=stream";
+
+const getInitObject = (data) => ({
+  method: "PUT",
+  headers: new Headers({
+    "Content-Type": "application/json",
+  }),
+  body: JSON.stringify(data),
+});
 export const updateCamera = async (data) => {
-  const init = {
-    method: "PUT",
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
-    body: JSON.stringify(data),
-  };
+  const init = getInitObject(data);
   const res = await fetch(backendUrl + "/robot/camera", init);
   return res.json();
 };
