fb64865a811f92b977a525d6b05b90977ad6c4a3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-24 09:04:47 +0200

    Adjust stream wrapper to fit max image possible

--- src/components/AppDisplay.js
+++ src/components/AppDisplay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 
 import "materialize-css";
@@ -12,8 +12,23 @@ import { cameraStatusReader } from "../hooks/useCameraState";
 
 import "./AppDisplay.css";
 
+const adjustedStreamWrapperSize = ({ current: streamRef }) => {
+  if (streamRef == null) {
+    return null;
+  }
+  const fittingRatio = Math.min(
+    (0.75 * window.innerWidth) / streamRef.naturalWidth,
+    (0.75 * window.innerHeight) / streamRef.naturalHeight
+  );
+  return {
+    width: streamRef.naturalWidth * fittingRatio || 0,
+    height: streamRef.naturalHeight * fittingRatio || 0,
+  };
+};
+
 const AppDisplay = ({ camera, dispatchCameraAction }) => {
   const isCamera = cameraStatusReader(camera);
+  const cameraStreamRef = useRef(null);
   const statusText = (() => {
     if (isCamera("starting") || isCamera("ready")) {
       return "Demarrage de la camera en cours...";
@@ -42,13 +57,19 @@ const AppDisplay = ({ camera, dispatchCameraAction }) => {
         </Navbar>
       </header>
       <div className="content row">
-        <div className="camera-stream-wrapper">
+        <div
+          className={`camera-stream-wrapper ${
+            isCamera("loaded") ? "smoothed" : ""
+          }`}
+          style={adjustedStreamWrapperSize(cameraStreamRef)}
+        >
           {(isCamera("ready") || isCamera("loaded")) && (
             <img
               className="camera-stream blue-grey lighten-4"
               alt="camera-stream"
               src={cameraStreamUrl}
               onLoad={() => dispatchCameraAction("stream-loaded")}
+              ref={cameraStreamRef}
             ></img>
           )}
           {!isCamera("loaded") && (
