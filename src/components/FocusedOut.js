368aad1ea9865bfd576fa715e56f574493e68661
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-24 19:03:03 +0200

    Create basic FocusedOut component

---
+++ src/components/FocusedOut.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./FocusedOut.css";
+
+const FocusedOut = () => {
+  return <div className="focused-out">Controle du robot perdu</div>;
+};
+
+export default FocusedOut;
