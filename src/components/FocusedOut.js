a55df3126db7af416caf84885efa5da75dfe0460
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-24 21:22:25 +0200

    Implement the focus out effect

--- src/components/FocusedOut.js
+++ src/components/FocusedOut.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import AppDisplay from "./AppDisplay";
 import "./FocusedOut.css";
 
-const FocusedOut = () => {
-  return <div className="focused-out">Controle du robot perdu</div>;
+const FocusedOut = ({ cameraStatus }) => {
+  const [clip, setClip] = useState(false);
+  useEffect(() => {
+    setTimeout(setClip, 0, true);
+  }, []);
+  return (
+    <div className="focused-out">
+      <AppDisplay
+        camera={cameraStatus}
+        dispatchCameraAction={() => null}
+        focused={true}
+        className={clip ? "clipped" : ""}
+      />
+    </div>
+  );
 };
 
+FocusedOut.protoTypes = {
+  cameraStatus: PropTypes.symbol.isRequired,
+};
 export default FocusedOut;
